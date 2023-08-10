9e26c78fa273ffaccf1a48730d7cd4f675324287
Alessandro Dias <alessandro.valerio.dias@gmail.com>
2023-08-10 11:59:38 -0300

    First Commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const merp-fvtt-unnoficial = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ merp-fvtt-unnoficial.abilities = {
+  "str": "merp-fvtt-unnoficial.AbilityStr",
+  "dex": "merp-fvtt-unnoficial.AbilityDex",
+  "con": "merp-fvtt-unnoficial.AbilityCon",
+  "int": "merp-fvtt-unnoficial.AbilityInt",
+  "wis": "merp-fvtt-unnoficial.AbilityWis",
+  "cha": "merp-fvtt-unnoficial.AbilityCha"
+};
+
+merp-fvtt-unnoficial.abilityAbbreviations = {
+  "str": "merp-fvtt-unnoficial.AbilityStrAbbr",
+  "dex": "merp-fvtt-unnoficial.AbilityDexAbbr",
+  "con": "merp-fvtt-unnoficial.AbilityConAbbr",
+  "int": "merp-fvtt-unnoficial.AbilityIntAbbr",
+  "wis": "merp-fvtt-unnoficial.AbilityWisAbbr",
+  "cha": "merp-fvtt-unnoficial.AbilityChaAbbr"
+};
